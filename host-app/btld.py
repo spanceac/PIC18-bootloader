6def08d4dbfb8886c4e429ca43fd307defa94947
Sebastian Panceac <spanceac@gmail.com>
2018-02-13 12:05:39 +0200

    Add error check and reporting to python flashing app

--- host-app/btld.py
+++ host-app/btld.py
@@ -1,20 +1,38 @@
 import serial
 import sys
 
-f = open(sys.argv[2], 'r')
-ser = serial.Serial(sys.argv[1], 115200, timeout = None)
+def check_if_err(recv):
+	if recv == "G":
+		print "Error: first instruction is not a GOTO, exiting"
+		sys.exit(1)
+	elif recv == "X":
+		print "Error: this hex overwrites the bootloader zone, exiting"
+		sys.exit(1)
+	elif recv == "E":
+		print "Error: MCU reported bad checksum, exiting"
+		sys.exit(1)
+	else:
+		return
 
-recv = '1' # initialise char
+def main():
+	recv = '1' # initialise char
+	f = open(sys.argv[2], 'r')
+	ser = serial.Serial(sys.argv[1], 115200, timeout = None)
 
-for line in f:
-	print 'Sending %s' % line,
-	ser.write(line,)
-	print 'Waiting for XON'
-	recv = ser.read()
-	while recv[0].encode('hex') != "11":
-		print 'We received 0x%s waiting for XON' % recv[0].encode('hex')
+	for line in f:
+		print 'Sending %s' % line,
+		ser.write(line,)
+		print 'Waiting for XON'
 		recv = ser.read()
-	print 'Received XON ... continuing'
+		while recv[0].encode('hex') != "11":
+			print 'We received 0x%s waiting for XON' % recv[0].encode('hex')
+			check_if_err(recv)
+			recv = ser.read()
+		print 'Received XON ... continuing'
 
-print 'Finnished sending hex'
-ser.close()
+	print 'Finnished sending hex'
+	ser.close()
+	sys.exit(0)
+
+if __name__ == "__main__":
+    main()
